1837ec5139b636d2dad3a658ef68baecc1004140
Spencer Clawson <Spencer@Datafest.com>
2019-07-31 16:22:55 -0600

    Add project files.

---
+++ KoalaCatcher/FileWatcher.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace KoalaCatcher
+{
+	public class FileWatcher
+	{
+		public FileWatcher(string directory, string fileType)
+		{
+			if (string.IsNullOrEmpty(directory))
+			{
+				Directory = System.IO.Directory.GetCurrentDirectory();
+			}
+			else if (!directory.Contains("\\\\") && !directory.Contains(":"))  //then it must be a relative path
+			{
+				Directory = System.IO.Directory.GetCurrentDirectory() + "\\" + directory.Replace("\\", "");
+			}
+			else
+			{
+				Directory = directory;
+			}
+
+			if (string.IsNullOrEmpty(fileType)) fileType = "*.txt";
+			if (!fileType.Contains(".")) fileType = "." + fileType;
+			if (!fileType.Contains("*")) fileType = "*" + fileType;
+			FileType = fileType;
+		}
+		public string Directory { get; }
+		public string FileType { get; }
+		
+		private bool _checking = false;
+		
+		private readonly Dictionary<string, FileLineCount> _fileLineCounts = new Dictionary<string, FileLineCount>();  //key is the file path
+		private class FileLineCount
+		{
+			public DateTime Modified;
+			public long LineCount;
+			public string OriginalCaseName;
+		}
+
+		/// <summary>
+		/// Checks the FileWatcher.Directory for changes to Directory.FileType files and returns the changes
+		/// </summary>
+		public string CheckFiles()
+		{
+			if (string.IsNullOrEmpty(Directory)) return string.Empty;
+
+			if (!System.IO.Directory.Exists(Directory)) return $"Can not access {Directory}, or it does not exist.";
+
+			if (_checking) return string.Empty;  //if the check takes long time we don't want to keep firing up threads before it's completed
+			_checking = true;
+			
+			var threads = new List<Thread>();
+			var result = new StringBuilder();
+		
+			try
+			{
+				//check for new or modified files
+				var files = System.IO.Directory.GetFiles(Directory, FileType);
+				foreach (var file in files)
+				{
+					FileLineCount fileLineCount;
+					var fileInfo = new FileInfo(file);
+					bool newFile = false, modified = false;
+
+					//exisiting files
+					if (_fileLineCounts.TryGetValue(file.ToUpper(), out fileLineCount))
+					{
+						if (fileInfo.LastWriteTime != fileLineCount.Modified)
+						{
+							modified = true;
+							fileLineCount.Modified = fileInfo.LastWriteTime;
+						}
+					}
+					//new files
+					else
+					{
+						newFile = true;
+						fileLineCount = new FileLineCount()
+						{
+							Modified = fileInfo.LastWriteTime,
+							OriginalCaseName = fileInfo.Name
+						};
+						_fileLineCounts.Add(file.ToUpper(), fileLineCount);
+						
+					}
+
+					if (newFile || modified)
+					{
+						var thread = new Thread(() =>
+						{
+							var lines = CountLines(file);
+							result.AppendLine(FormatResult(newFile ? FileStatus.New : FileStatus.Exisiting, fileInfo.Name, fileLineCount.LineCount, lines));
+							fileLineCount.LineCount = lines;
+						});
+						thread.Start();
+						threads.Add(thread);
+					}
+				}
+
+				// check for deleted files
+				var upperFiles = files.Select(f => f.ToUpper()).ToArray();
+				var deleted = new List<string>();
+				foreach (var fileStat in _fileLineCounts)
+				{
+					if (!upperFiles.Contains(fileStat.Key))
+					{
+						result.AppendLine(FormatResult(FileStatus.Deleted, fileStat.Value.OriginalCaseName, 0, 0));
+						deleted.Add(fileStat.Key);
+					}
+				}
+				
+				foreach (var file in deleted)
+				{
+					_fileLineCounts.Remove(file);
+				}
+
+
+				foreach (var thread in threads)
+				{
+					thread.Join();
+				}
+			}
+			catch (Exception ex)
+			{
+				//Log ex
+				result.AppendLine("Error: " + ex.Message);
+			}
+
+			_checking = false;
+			return result.ToString();
+		}
+
+		
+		
+		public enum FileStatus
+		{
+			New, Exisiting, Deleted
+		}
+		public static string FormatResult(FileStatus fileStatus, string name, long oldCount, long newCount)
+		{
+			switch (fileStatus)
+			{
+				case FileStatus.New:
+					return $"{name} {newCount}";
+				case FileStatus.Exisiting:
+					var lines = newCount - oldCount;
+					var plusSign = lines > 0 ? "+" : "";
+					return $"{name} {plusSign}{lines}";
+				case FileStatus.Deleted:
+					return name;
+			}
+
+			return string.Empty;
+		}
+		
+
+		public static long CountLines(string file)
+		{
+			if (string.IsNullOrEmpty(file)) return 0;
+
+			long count = 0;
+
+			try
+			{
+				using (var stream = OpenFile(file))
+				{
+					while (stream.ReadLine() != null)
+					{
+						count++;
+					}
+				}				
+			}
+			catch (Exception ex)
+			{
+				//Log ex
+				Console.WriteLine(ex.Message);
+			}
+
+			return count;
+		}
+
+		/// <summary>
+		/// Tries for up to 5 seconds to open the given file
+		/// </summary>
+		/// <exception cref="Exception">If the file is unable to be opened</exception>
+		public static StreamReader OpenFile(string file)
+		{
+			const int timeoutSeconds = 5;
+
+			for (int i = 0; i < timeoutSeconds * 2; i++)
+			{
+				try
+				{
+					return new StreamReader(file);
+				}
+				catch
+				{
+					Thread.Sleep(500);
+				}
+			}
+
+			throw new TimeoutException($"Unable to open file {file} for {timeoutSeconds} seconds");
+		}
+	}
+}
---
+++ KoalaCatcher/FileWatcherTest.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using NUnit.Framework;
+
+namespace KoalaCatcherTest
+{
+	[TestFixture]
+	public class FileWatcher
+	{
+		/*
+		 * The program takes 2 arguments, the directory to watch and a file pattern, example: program.exe "c:\file folder" *.txt
+		
+		The path may be an absolute path, relative to the current directory, or UNC.
+		Use the modified date of the file as a trigger that the file has changed.
+		Check for changes every 10 seconds.
+		When a file is created output a line to the console with its name and how many lines are in it.
+		When a file is modified output a line with its name and the change in number of lines (use a + or - to indicate more or less).
+		When a file is deleted output a line with its name.
+		Files will be ASCII or UTF-8 and will use Windows line separators (CR LF).
+		Multiple files may be changed at the same time, can be up to 2 GB in size, and may be locked for several seconds at a time.
+		Use multiple threads so that the program doesn't block on a single large or locked file.
+		Program will be run on Windows 10.
+		File names are case insensitive.
+		 */
+
+		[Test]
+		public void CheckFiles()
+		{
+			CheckFiles("","");
+			CheckFiles("","txt");
+			CheckFiles("",".txt");
+			CheckFiles("","*.txt");
+			CheckFiles("","*.csv");
+			CheckFiles(Path.GetTempPath(), "*.txt");
+			CheckFiles("\\\\guppy\\pub\\spencer\\","");
+			Directory.SetCurrentDirectory("C:\\users");
+			CheckFiles("Spencer","");
+			
+		}
+
+		public void CheckFiles(string directory, string fileType)
+		{
+			var fileWatcher = new KoalaCatcher.FileWatcher(directory, fileType);
+			fileWatcher.CheckFiles();
+
+			var file = CreateTestFile(400);
+			var info = new FileInfo(file);
+			var renamedFile = $"{fileWatcher.Directory}\\{info.Name.Replace(info.Extension,"")}{fileWatcher.FileType.Replace("*","")}";
+
+			info.MoveTo(renamedFile);
+			Assert.AreEqual($"{info.Name} 400\r\n", fileWatcher.CheckFiles());
+			
+			using (var writer = File.AppendText(renamedFile))
+			{
+				writer.WriteLine();
+			}
+			Assert.AreEqual($"{info.Name} +1\r\n", fileWatcher.CheckFiles());
+			
+			File.Delete(renamedFile);
+			Assert.AreEqual($"{info.Name}\r\n", fileWatcher.CheckFiles());
+		}
+
+		[Test]
+		public void OpenFile()
+		{
+			var file = CreateTestFile(20);
+
+			//does it open files
+			using (var stream = KoalaCatcher.FileWatcher.OpenFile(file))
+			{
+				Assert.IsNotNull(stream);
+			}
+
+			//if we lock the file for a few seconds, will it still open
+			Thread lockThread = new Thread(new ThreadStart(() =>
+			{
+				using (var stream = new FileStream(file, FileMode.Open, FileAccess.Read, FileShare.None))
+				{
+					Thread.Sleep(2000);
+				}
+			}));
+			Thread testThread = new Thread(new ThreadStart(() =>
+			{
+				Assert.DoesNotThrow(() =>
+				{
+					using (var stream = KoalaCatcher.FileWatcher.OpenFile(file))
+					{
+						Assert.IsNotNull(stream);
+					}
+				});
+				
+			}));
+			lockThread.Start();
+			testThread.Start();
+			lockThread.Join();
+			testThread.Join();
+
+
+			//if we lock the file for a "long" time, it will fail
+			lockThread = new Thread(new ThreadStart(() =>
+			{
+				using (var stream = new FileStream(file, FileMode.Open, FileAccess.Read, FileShare.None))
+				{
+					Thread.Sleep(10000);
+				}
+			}));
+			testThread = new Thread(new ThreadStart(() =>
+			{
+				Assert.Catch(() =>
+				{
+					using (var stream = KoalaCatcher.FileWatcher.OpenFile(file)) { }
+				});
+				
+			}));
+			lockThread.Start();
+			testThread.Start();
+			lockThread.Join();
+			testThread.Join();
+			
+
+			File.Delete(file);
+		}
+
+		[Test]
+		public void CountLines()
+		{
+			var file = CreateTestFile(20);
+			Assert.AreEqual(20L, KoalaCatcher.FileWatcher.CountLines(file));
+			File.Delete(file);
+
+			file = CreateTestFile(4000000);
+			var start = DateTime.Now;
+			Assert.AreEqual( 4000000L, KoalaCatcher.FileWatcher.CountLines(file));
+			
+			var info = new FileInfo(file);
+			Console.WriteLine($"It took {(DateTime.Now - start).TotalMilliseconds} ms to count the lines in a {info.Length / 1024} KB file.");
+
+			File.Delete(file);
+		}
+
+		[Test]
+		public void FormatResult()
+		{
+			Assert.AreEqual(
+				"file1",
+				KoalaCatcher.FileWatcher.FormatResult(KoalaCatcher.FileWatcher.FileStatus.Deleted, "file1", 0, 0));
+
+			Assert.AreEqual(
+				"file1 100",
+				KoalaCatcher.FileWatcher.FormatResult(KoalaCatcher.FileWatcher.FileStatus.New, "file1", 0, 100));
+
+			Assert.AreEqual(
+				"file1 +10",
+				KoalaCatcher.FileWatcher.FormatResult(KoalaCatcher.FileWatcher.FileStatus.Exisiting, "file1", 90, 100));
+
+			Assert.AreEqual(
+				"file1 -10",
+				KoalaCatcher.FileWatcher.FormatResult(KoalaCatcher.FileWatcher.FileStatus.Exisiting, "file1", 100, 90));
+		}
+
+
+		
+
+		static string CreateTestFile(int numberOfLines)
+		{
+			var file = Path.GetTempFileName();
+
+			string g = string.Join("", Enumerable.Repeat(new Guid().ToString("n"), 20));
+
+			try
+			{
+				using (StreamWriter sw = File.AppendText(file))
+				{
+					for (int x = 0; x < numberOfLines; x++)
+					{
+						sw.WriteLine(g);
+					}
+				}
+			}
+			catch (Exception ex)
+			{
+				
+			}
+
+			return file;
+		}
+	}
+}
